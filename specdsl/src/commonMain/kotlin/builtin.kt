0e2231191490cc41af49af64b7326cb02eeb6356
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2024-06-27 15:05:49 +0300

    breaking: changed builtin types
    1. removed builtin.Nothing
    1. added builtin.Any
    1. builtin.NULL is now of type Any.optional

--- specdsl/src/commonMain/kotlin/builtin.kt
+++ specdsl/src/commonMain/kotlin/builtin.kt
@@ -20,21 +20,21 @@ package org.cufy.specdsl
  */
 @Suppress("ClassName")
 object builtin : NamespaceObject() {
-    val Nothing by nativeScalar { +"builtin nothing (the type of null)" }
-    val NULL by const("null", Nothing) { +"builtin null" }
-    val String by nativeScalar { +"builtin string" }
-    val Boolean by nativeScalar { +"builtin boolean" }
+    val Any by scalar { +"Everything extends Any" }
+    val NULL by const("null", Any.optional) { +"builtin null" }
+    val String by scalar { +"builtin string" }
+    val Boolean by scalar { +"builtin boolean" }
     val TRUE by const("true", Boolean) { +"builtin true" }
     val FALSE by const("false", Boolean) { +"builtin false" }
-    val Int32 by nativeScalar { +"builtin 32bit int" }
-    val UInt32 by nativeScalar { +"builtin unsigned 32bit int" }
-    val Int64 by nativeScalar { +"builtin 64bit int" }
-    val UInt64 by nativeScalar { +"builtin unsigned 64bit int" }
-    val Float32 by nativeScalar { +"builtin 32bit float" }
-    val Float64 by nativeScalar { +"builtin 64bit float" }
+    val Int32 by scalar { +"builtin 32bit int" }
+    val UInt32 by scalar { +"builtin unsigned 32bit int" }
+    val Int64 by scalar { +"builtin 64bit int" }
+    val UInt64 by scalar { +"builtin unsigned 64bit int" }
+    val Float32 by scalar { +"builtin 32bit float" }
+    val Float64 by scalar { +"builtin 64bit float" }
 
-    val Deprecated__message by metadata_param { +"The deprecation message" }
-    val Deprecated by nativeMetadata(Deprecated__message)
+    val Deprecated__message by metadata_param(String) { +"The deprecation message" }
+    val Deprecated by metadata(Deprecated__message)
 
     /**
      * Namespace to be used for defining builtin faults.
