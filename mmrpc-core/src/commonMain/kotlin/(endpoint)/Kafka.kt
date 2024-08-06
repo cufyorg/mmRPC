94d7cee8aedd87458d7c20ca60deb98c4c947dc0
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2024-08-06 19:44:50 +0300

    refactor(core): changed kafka security spec

--- mmrpc-core/src/commonMain/kotlin/(endpoint)/Kafka.kt
+++ mmrpc-core/src/commonMain/kotlin/(endpoint)/Kafka.kt
@@ -66,8 +66,16 @@ object Kafka {
      * The jwt should include the header `kid` which should be
      * the id of either a previously agreed upon key of the
      * client or a key in a previously agreed upon keyset uri.
-     * Additionally, the jwt should include the claim `client_id`
-     * which should be the id of the client.
+     *
+     * Additionally, the jwt should include the claim `iss`
+     * which should be the id of the software.
+     *
+     * If the jwt has the header `cty` is present, it must be one of:
+     *
+     * - `jwt`
+     * - `application/jwt`
+     * - `mmrpc-event+jwt`
+     * - `application/mmrpc-event+jwt`
      */
     val SameClient = KafkaSecurity("SameClient")
 
@@ -82,8 +90,16 @@ object Kafka {
      * The jwt should include the header `kid` which should be
      * the id of either a previously agreed upon key of the
      * software or a key in a previously agreed upon keyset uri.
-     * Additionally, the jwt should include the claim `software_id`
+     *
+     * Additionally, the jwt should include the claim `iss`
      * which should be the id of the software.
+     *
+     * If the jwt has the header `cty` is present, it must be one of:
+     *
+     * - `jwt`
+     * - `application/jwt`
+     * - `mmrpc-event+jwt`
+     * - `application/mmrpc-event+jwt`
      */
     val SameSoftware = KafkaSecurity("SameSoftware")
 
