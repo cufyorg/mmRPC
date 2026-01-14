08c923e0b59c6417628bbf4840e7da8aa44c10cd
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2026-01-14 07:34:23 +0300

    feat: allow null assignment onto Box and no null in Unnamed

--- mmrpc-definition/src/commonMain/kotlin/Box.kt
+++ mmrpc-definition/src/commonMain/kotlin/Box.kt
@@ -15,12 +15,12 @@ fun <T : Any> KProperty0<Box<T>>.getOrNull(): T? =
 fun <T : Any> KProperty0<Box<T>>.getOrThrow(): T =
     this.get().value ?: error("Required value `${this.name}` was not set")
 
-operator fun <T : Any> Box<T>.timesAssign(value: T) =
+operator fun <T : Any> Box<T>.timesAssign(value: T?) =
     run { this.value = value }
 
 @JvmName("Box_Unnamed_T_timesAssign")
-operator fun <T> Box<Unnamed<T>>.timesAssign(value: T) =
-    run { this.value = Unnamed(value) }
+operator fun <T : Any> Box<Unnamed<T>>.timesAssign(value: T?) =
+    run { this.value = if (value == null) null else Unnamed(value) }
 
-operator fun Box<Literal>.timesAssign(value: ConstDefinition) =
-    run { this.value = value.value }
+operator fun Box<Literal>.timesAssign(value: ConstDefinition?) =
+    run { this.value = value?.value }
--- mmrpc-definition/src/commonMain/kotlin/Unnamed.kt
+++ mmrpc-definition/src/commonMain/kotlin/Unnamed.kt
@@ -2,11 +2,11 @@ package org.cufy.mmrpc
 
 import kotlin.reflect.KProperty
 
-interface Unnamed<out T> {
+interface Unnamed<out T : Any> {
     fun get(ns: CanonicalName?, name: String?): T
 }
 
-fun <T> Unnamed(value: T): Unnamed<T> {
+fun <T : Any> Unnamed(value: T): Unnamed<T> {
     return object : Unnamed<T> {
         override fun get(ns: CanonicalName?, name: String?): T {
             return value
@@ -14,7 +14,7 @@ fun <T> Unnamed(value: T): Unnamed<T> {
     }
 }
 
-fun <T> Unnamed(block: (ns: CanonicalName?, name: String?) -> T): Unnamed<T> {
+fun <T : Any> Unnamed(block: (ns: CanonicalName?, name: String?) -> T): Unnamed<T> {
     val cache = mutableMapOf<Pair<CanonicalName?, String?>, T>()
     return object : Unnamed<T> {
         override fun get(ns: CanonicalName?, name: String?): T {
@@ -25,29 +25,29 @@ fun <T> Unnamed(block: (ns: CanonicalName?, name: String?) -> T): Unnamed<T> {
     }
 }
 
-fun <T> Unnamed<T>.get(obj: NamespaceObject, name: String?) = get(obj.canonicalName, name)
+fun <T : Any> Unnamed<T>.get(obj: NamespaceObject, name: String?) = get(obj.canonicalName, name)
 
-operator fun <T> Unnamed<T>.getValue(obj: NamespaceObject, property: KProperty<*>): T {
+operator fun <T : Any> Unnamed<T>.getValue(obj: NamespaceObject, property: KProperty<*>): T {
     val splits = property.name.split("__")
     val ns = obj.canonicalName + splits.dropLast(1)
     val n = splits.last()
     return get(ns, n)
 }
 
-operator fun <T> Unnamed<T>.getValue(obj: Any?, property: KProperty<*>): T {
+operator fun <T : Any> Unnamed<T>.getValue(obj: Any?, property: KProperty<*>): T {
     val splits = property.name.split("__")
     val ns = CanonicalName(splits.dropLast(1))
     val n = splits.last()
     return get(ns, n)
 }
 
-operator fun <T> List<Unnamed<T>>.getValue(obj: NamespaceObject, property: KProperty<*>): List<T> {
+operator fun <T : Any> List<Unnamed<T>>.getValue(obj: NamespaceObject, property: KProperty<*>): List<T> {
     val splits = property.name.split("__")
     val ns = obj.canonicalName + splits
     return map { it.get(ns, name = null) }
 }
 
-operator fun <T> List<Unnamed<T>>.getValue(obj: Any?, property: KProperty<*>): List<T> {
+operator fun <T : Any> List<Unnamed<T>>.getValue(obj: Any?, property: KProperty<*>): List<T> {
     val splits = property.name.split("__")
     val ns = CanonicalName(splits)
     return map { it.get(ns, name = null) }
