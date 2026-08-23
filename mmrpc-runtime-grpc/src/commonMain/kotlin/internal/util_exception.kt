134d1b9565bc7d959c5c36d27631f5a1c9f9471b
lsafer-meemer <135058778+lsafer-meemer@users.noreply.github.com>
2026-08-23 11:05:51 +0300

    fix(runtime-grpc): fault-object to status exception mapping

--- mmrpc-runtime-grpc/src/commonMain/kotlin/internal/util_exception.kt
+++ mmrpc-runtime-grpc/src/commonMain/kotlin/internal/util_exception.kt
@@ -2,12 +2,14 @@ package org.cufy.mmrpc.runtime.grpc.internal
 
 import com.google.protobuf.Any
 import com.google.rpc.Status
+import io.grpc.Status.Code
 import io.grpc.StatusRuntimeException
 import io.grpc.protobuf.StatusProto
 import org.cufy.mmrpc.runtime.FaultObject
 
 internal fun FaultObject.toStatusRuntimeException(): StatusRuntimeException {
     val status = Status.newBuilder()
+        .setCode(Code.UNKNOWN.value())
         .setMessage(this.message.orEmpty())
         .addDetails(
             Any.newBuilder()
@@ -20,6 +22,7 @@ internal fun FaultObject.toStatusRuntimeException(): StatusRuntimeException {
 
 internal fun StatusRuntimeException.toFaultObjectOrNull(): FaultObject? {
     val status = StatusProto.fromThrowable(this) ?: return null
-    val detail = status.detailsList.firstOrNull() ?: return null
+    if (status.code != Code.UNKNOWN.value()) return null
+    val detail = status.detailsList.singleOrNull() ?: return null
     return FaultObject(detail.typeUrl, status.message)
 }
